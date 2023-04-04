aa69e2a33e807cec43e3e1c7de483aa264cb94bd
최가람 <riverchoi2523@gmail.com>
2023-04-04 13:24:08 +0900

    fe

--- nodejs/web2-nodejs/nodejs/fileread.js
+++
@@ -1,7 +0,0 @@
-//nodejs의 모듈 중 파일 시스템 호출
-var fs = require('fs')
-fs.readFile('sample.txt', 'utf8',function(err, data){
-  console.log(data);
-})
-//상위 디렉토리에서 이걸 실행 시 sample.txt를 상위 디렉토리에서 찾는다.
-//그러므로 디렉토리를 바꾸고 실행해야 함
--- nodejs/web2-nodejs/syntax/string.js
+++
@@ -1,2 +0,0 @@
-console.log('1'+'1');
-console.log('hello nice to meet you i love your name how sweet! today is the best day ever..'.length);
